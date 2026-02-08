5347a6ef92baf03f2865065b3ea2699ae87fe490
Łukasz Gryczka <lenternus@gmail.com>
2026-02-08 12:22:03 +0100

    Cleanup Code

--- src/main/java/pl/luki2183/farmManager/FarmManagerApplication.java
+++ src/main/java/pl/luki2183/farmManager/FarmManagerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FarmManagerApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(FarmManagerApplication.class, args);
 	}
 
