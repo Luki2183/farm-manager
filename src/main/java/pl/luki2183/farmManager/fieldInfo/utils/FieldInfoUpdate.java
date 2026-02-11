74bb1d3c888b5443485ef3f33490e318a4562165
Łukasz Gryczka <lenternus@gmail.com>
2026-02-11 12:12:43 +0100

    Create FieldInfoUpdate.java

---
+++ src/main/java/pl/luki2183/farmManager/fieldInfo/utils/FieldInfoUpdate.java
@@ -0,0 +1,35 @@
+package pl.luki2183.farmManager.fieldInfo.utils;
+
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.luki2183.farmManager.fieldInfo.dto.FieldInfoDto;
+import pl.luki2183.farmManager.fieldInfo.model.Color;
+import pl.luki2183.farmManager.fieldInfo.model.FieldInfoEntity;
+import pl.luki2183.farmManager.fieldInfo.model.Grain;
+import pl.luki2183.farmManager.utils.DateFormat;
+import pl.luki2183.farmManager.weatherInfo.model.WeatherInfoEntity;
+
+import java.time.LocalDate;
+
+@Component
+@AllArgsConstructor
+public class FieldInfoUpdate {
+
+    private final DateFormat dateFormat;
+    private final ColorParser colorParser;
+
+    @Transactional
+    public FieldInfoEntity update(FieldInfoEntity entity, FieldInfoDto dto) {
+        entity.setSurfaceArea(dto.getSurfaceArea());
+        entity.setGrainType(Grain.valueOf(dto.getGrainType()));
+        entity.setPlantDate(LocalDate.parse(dto.getPlantDate(), dateFormat.getDateFormat()));
+        entity.setExpectedHarvestDate(LocalDate.parse(dto.getExpectedHarvestDate(), dateFormat.getDateFormat()));
+        WeatherInfoEntity weatherInfo = new WeatherInfoEntity();
+        weatherInfo.setHumidity(dto.getHumidity());
+        weatherInfo.setWindSpeed(dto.getWindSpeed());
+        entity.setWeatherInfo(weatherInfo);
+        entity.setFieldColor(colorParser.parse(dto.getFieldColor()));
+        return entity;
+    }
+}
