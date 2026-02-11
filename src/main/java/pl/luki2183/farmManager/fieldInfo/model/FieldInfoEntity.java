16727200f5b376586050b6f236d067c2231947e4
Łukasz Gryczka <lenternus@gmail.com>
2026-02-11 12:11:43 +0100

    Update FieldInfoEntity.java

--- src/main/java/pl/luki2183/farmManager/fieldInfo/model/FieldInfoEntity.java
+++ src/main/java/pl/luki2183/farmManager/fieldInfo/model/FieldInfoEntity.java
@@ -5,7 +5,6 @@ import lombok.*;
 import pl.luki2183.farmManager.fields.model.FieldEntity;
 import pl.luki2183.farmManager.weatherInfo.model.WeatherInfoEntity;
 
-import java.awt.*;
 import java.time.LocalDate;
 
 @Entity
