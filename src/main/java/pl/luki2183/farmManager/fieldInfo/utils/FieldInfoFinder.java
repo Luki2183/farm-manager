bfe04acd9448f81b4534812db044dafdedf9d294
Łukasz Gryczka <lenternus@gmail.com>
2026-02-11 11:47:26 +0100

    Update FieldInfoFinder todo

--- src/main/java/pl/luki2183/farmManager/fieldInfo/utils/FieldInfoFinder.java
+++ src/main/java/pl/luki2183/farmManager/fieldInfo/utils/FieldInfoFinder.java
@@ -6,6 +6,7 @@ import pl.luki2183.farmManager.utils.Finder;
 public class FieldInfoFinder implements Finder<FieldInfoEntity, String> {
     @Override
     public FieldInfoEntity find(String s) {
+//        todo update when necessary, in other case delete
         return null;
     }
 }
