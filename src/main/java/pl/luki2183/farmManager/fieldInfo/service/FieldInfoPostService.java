0f65b9e2f110515025bc05f315f01fbe3922aed3
Łukasz Gryczka <lenternus@gmail.com>
2026-02-11 11:45:14 +0100

    Updated FieldFinder logic and usages

--- src/main/java/pl/luki2183/farmManager/fieldInfo/service/FieldInfoPostService.java
+++ src/main/java/pl/luki2183/farmManager/fieldInfo/service/FieldInfoPostService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import pl.luki2183.farmManager.fieldInfo.dto.FieldInfoDto;
 import pl.luki2183.farmManager.fieldInfo.mapper.FieldInfoMapper;
 import pl.luki2183.farmManager.fieldInfo.model.FieldInfoEntity;
+import pl.luki2183.farmManager.fields.utils.FieldFinder;
 import pl.luki2183.farmManager.weatherInfo.model.WeatherInfoEntity;
 import pl.luki2183.farmManager.fieldInfo.repo.FieldInfoRepository;
 import pl.luki2183.farmManager.fields.model.FieldEntity;
@@ -17,13 +18,14 @@ import pl.luki2183.farmManager.weatherInfo.service.WeatherGetService;
 public class FieldInfoPostService {
 
     private final FieldInfoRepository repository;
-    private final FieldRepository fieldRepository;
     private final WeatherGetService weatherGetService;
     private final FieldInfoMapper mapper;
+    private final FieldFinder fieldFinder;
 
     @Transactional
     public FieldInfoEntity addInfo(FieldInfoDto dto) {
-        FieldEntity fieldEntity = fieldRepository.getReferenceById(dto.getFieldId());
+        FieldEntity fieldEntity = fieldFinder.find(dto.getFieldId());
+//        todo reformat to not use weatherGetService in another service
         WeatherInfoEntity weatherInfo = weatherGetService.getWeatherInfo(fieldEntity.getCoordinates().getFirst());
         FieldInfoEntity entity = mapper.dtoToInfo(dto, fieldEntity, weatherInfo);
         return repository.save(entity);
--- src/main/java/pl/luki2183/farmManager/fields/utils/FieldFinder.java
+++ src/main/java/pl/luki2183/farmManager/fields/utils/FieldFinder.java
@@ -1,11 +1,20 @@
 package pl.luki2183.farmManager.fields.utils;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
 import pl.luki2183.farmManager.fields.model.FieldEntity;
+import pl.luki2183.farmManager.fields.repo.FieldRepository;
 import pl.luki2183.farmManager.utils.Finder;
 
+@Component
+@AllArgsConstructor
 public class FieldFinder implements Finder<FieldEntity, String> {
+
+    private final FieldRepository repository;
+
     @Override
-    public FieldEntity find(String s) {
-        return null;
+    public FieldEntity find(String id) {
+//        todo add notFoundExceptions
+        return repository.getReferenceById(id);
     }
 }
